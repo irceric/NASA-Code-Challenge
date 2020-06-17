a958d483f4bcdf9ee6771a1469fdebc89173fdf6
Eric Chen <ic.eric.chen83@gmail.com>
2020-06-17 00:29:40 -0500

    some styles

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React, { useState, useEffect, useCallback, useRef } from 'react';
 
 import DateInput from 'components/DateInput';
-import { headerStyle } from 'styles';
+import { headerStyle, imageStyle, videoPlayerStyle } from 'styles';
 import { randomDate } from 'helpers/date';
 import { getAPOD } from 'services';
 
 const App = () => {
   const [date, setDate] = useState(new Date());
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+  const [data, setData] = useState(null);
+
   const startDate = useRef(new Date(1995, 6, 16));
 
   const handleSetRandomDate = () => {
@@ -14,8 +18,15 @@ const App = () => {
   };
 
   const fetchData = useCallback(async (date) => {
-    const response = await getAPOD(date);
-    console.log(response);
+    setLoading(true);
+    try {
+      const response = await getAPOD(date);
+      setData(response);
+    } catch (err) {
+      setError(err);
+    } finally {
+      setLoading(false);
+    }
   }, []);
 
   useEffect(() => {
@@ -24,24 +35,44 @@ const App = () => {
 
   return (
     <div className='container'>
-      <div className='card card-body'>
-        <h2 style={headerStyle}>NASA's Picture of the Day</h2>
-      </div>
-      <div className='card card-body'>
-        <DateInput
-          date={date}
-          onChange={(date) => setDate(date)}
-          onRandomDate={handleSetRandomDate}
-        />
-      </div>
       <div className='card'>
+        <h2 style={headerStyle}>NASA's Picture of the Day</h2>
         <div className='card-body'>
-          <h5 className='card-title'>Card title</h5>
-          <p className='card-text'>
-            Some quick example text to build on the card title and make up the
-            bulk of the card's content.
-          </p>
+          <DateInput
+            date={date}
+            onChange={(date) => setDate(date)}
+            onRandomDate={handleSetRandomDate}
+          />
         </div>
+        {loading && <h3>Loading...</h3>}
+        {error && (
+          <div class='alert alert-danger' role='alert'>
+            {error}
+          </div>
+        )}
+        {data && (
+          <div className='card-body'>
+            <h5 className='card-title text-center'>{data.title}</h5>
+            {data.media_type === 'image' ? (
+              <img
+                className='card-img-top'
+                style={imageStyle}
+                src={data.url}
+                alt='alt'
+              />
+            ) : (
+              <div className='video' style={videoPlayerStyle}>
+                <iframe
+                  style={videoPlayerStyle}
+                  title='video'
+                  src={data.url}
+                  frameBorder='0'
+                />
+              </div>
+            )}
+            <p className='card-text mt-2'>{data.explanation}</p>
+          </div>
+        )}
       </div>
     </div>
   );
--- src/styles/index.js
+++ src/styles/index.js
@@ -2,3 +2,16 @@ export const headerStyle = {
   textShadow: '1px 2px #282794',
   textAlign: 'center',
 };
+
+export const imageStyle = {
+  maxHeight: '300px',
+  objectFit: 'cover',
+};
+
+export const videoPlayerStyle = {
+  position: 'absolute',
+  top: 0,
+  left: 0,
+  width: '100%',
+  height: '100%',
+};
